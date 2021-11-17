7d62eb185e17022be28084ef14e1ddd23ac2d1de
[LHM] <[gpalsdl6369@naver.com]>
2021-11-17 15:16:55 +0900

    java-study

---
+++ HelloWorld/src/hello/App.java
@@ -0,0 +1,16 @@
+package hello; // 패키지 헬로우
+
+public class App { // 클래스 App 만들기
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 메인 메소드에서 프로그램 시작
+		// 주석 단축키 ctrl + /
+		System.out.println("헬로우");
+	    // 실행 방법 이클상단 ▶ , 오른쪽 마우스 Run as 자바 애플리케이션
+		// 왼쪽편 클래스 파일을 선택하고 ▶ 오른쪽 마우스 Run as 자바 애플리케이션 
+		// ctrl + F11 ▶
+        
+	}
+
+}
